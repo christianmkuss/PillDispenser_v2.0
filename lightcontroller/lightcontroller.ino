f42a2562874ea7c935e5c5d3ec45fbcc9acb8f78
christianmkuss <33375981+christianmkuss@users.noreply.github.com>
2018-07-14 20:41:28 +0400

    added customization over bluetooth

--- lightcontroller/lightcontroller.ino
+++ lightcontroller/lightcontroller.ino
@@ -22,8 +22,12 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
+// Global variable for the value of the string so it doesn't get set to blank everytime
+String user = "";
+
 void setup() {
   Serial.begin(9600);
+  Serial2.begin(9600);
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
@@ -38,14 +42,62 @@ void setup() {
 void loop() {
   // Initialize the input to ""
   String value = "";
-
+  if (Serial2.available()) {
+    user = Serial2.readString();
+  }
   if (Serial.available()) {
     value = Serial.readString();
     if (value == "y") {
       dispensed();
     }
   }
-  rainbowCycle(50);
+  if (user == "1") {
+    rainbowCycle(50);
+  } else if (user == "2") {
+    colorWipe(strip.Color(255, 0, 0), 50); // Red
+    colorWipe(strip.Color(0, 255, 0), 50); // Green
+    colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  } else if (user == "3") {
+    theaterChase(strip.Color(127, 127, 127), 50); // White
+    theaterChase(strip.Color(127, 0, 0), 50); // Red
+    theaterChase(strip.Color(0, 0, 127), 50); // Blue
+  } else {
+    theaterChaseRainbow(50);
+  }
+}
+
+void dispensed() {
+  for (int j = 0; j < 15; j++) {
+    for (int i = 0; i < NUMPIXELS; i++) {
+      strip.setPixelColor(i, 255, 0, 0);
+    }
+    delay(750);
+    for (int i = 0; i < NUMPIXELS; i++) {
+      strip.setPixelColor(i, 0, 0, 0);
+    }
+    delay(750);
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
@@ -61,16 +113,39 @@ void rainbowCycle(uint8_t wait) {
   }
 }
 
-void dispensed() {
-  for (int j = 0; j < 15; j++) {
-    for (int i = 0; i < NUMPIXELS; i++) {
-      strip.setPixelColor(i, 255, 0, 0);
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
     }
-    delay(750);
-    for (int i = 0; i < NUMPIXELS; i++) {
-      strip.setPixelColor(i, 0, 0, 0);
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
+    for (int q = 0; q < 3; q++) {
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
+      }
     }
-    delay(750);
   }
 }
 
